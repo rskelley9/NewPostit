dfd52e9214d4fc9d81b3045f8b3c6a6b22387285
Ryan Kelley <rskelley9@gmail.com>
2014-01-31 00:19:10 -0500

    added erase functionality

--- post-it.js
+++ post-it.js
@@ -1,26 +1,8 @@
-// var Board = function( selector ) {
-//   // Your board related code goes here
-
-//   // Use $elem to access the DOM element for this board
-//   var $elem = $( selector );
-
-//   function initialize() {
-//     // What needs to happen when this object is created?
-//   };
-
-//   initialize();
-// };
-
-// var PostIt = function() {
-//   // Your post-it related code goes here
-// };
-
-
 var getAction = function(actions) {
  return actions[Math.floor(Math.random() * actions.length)];
 }
 
-var changePostItPos = function(id,x,y) {
+var movePostIt = function(id,x,y) {
   $("#"+id).css("top",y);
   $("#"+id).css("left",x);
 };
@@ -32,7 +14,7 @@ var showPostIt = function(id,array) {
 }
 
 
-var increment = {
+var count = {
   value: 0,
   count: function() {
     this.value = this.value + 1
@@ -45,9 +27,9 @@ function initialize() {
 
   $(document).click(function(event) {
     //if there click target isn't a post-it, nor the span.close area then initialize postit
-    if ((($(event.target).hasClass("post-it")) === false) && (($(event.target).hasClass("header")) === false) && (($(event.target).hasClass("content")) === false) && (($(event.target).hasClass("close")) === false)){
+    if ((($(event.target).hasClass("post-it")) === false) && (($(event.target).hasClass("header")) === false) && (($(event.target).hasClass("content")) === false) && (($(event.target).hasClass("close")) === false) && (($(event.target).hasClass("erase")) === false)){
 
-      id = increment.value
+      id = count.value
 
       newPostIt = "<div class='header'><span class='erase'>E</span><span class='close'>X</span></div>"
 
@@ -59,19 +41,25 @@ function initialize() {
 
       $("#"+id).append("<div class='content' contenteditable='true'>")
 
-      changePostItPos(id,event.clientX,event.clientY)
+      movePostIt(id,event.clientX,event.clientY)
 
       actions = ["clip","blind","bounce","explode","highlight", "shake", "scale", "pulsate", "slide", "size", "fold", "puff"]
 
       showPostIt(id,actions)
 
-      increment.count()
+      count.count()
     }
-    else if ($(event.target).hasClass("close") === true) {
-      // $(event.target).parent().parent().remove()
-      $(event.target).parents(":eq(1)").fadeOut({duration:1000})
-      $(event.target).parents(":eq(1)").fadeIn({duration:1000})
+    else if ($(event.target).hasClass("close") === true)
+    {
+      $(event.target).parents(":eq(1)").fadeOut({duration:1100})
+      // $(event.target).parents(":eq(1)").fadeIn({duration:1000})
       //erase text with fadeOut on click
+    }
+
+    else if ($(event.target).hasClass("erase") === true)
+    {
+      $(event.target).parent().siblings().first().wrapInner( "<div class='fade'></div>" )
+      $('.fade').fadeOut({duration:800})
     }
     else {
       $(event.target).focus()
