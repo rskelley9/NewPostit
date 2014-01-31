6d80a228f1650fb4c559c28d5e7ec1b472ee56be
Ryan Kelley <rskelley9@gmail.com>
2014-01-30 23:11:33 -0500

    styled postits adding erase text

--- post-it.js
+++ post-it.js
@@ -43,22 +43,23 @@ var increment = {
 
 function initialize() {
 
-  $(document).click(function(e) {
+  $(document).click(function(event) {
     //if there click target isn't a post-it, nor the span.close area then initialize postit
-    if ((($(e.target).hasClass("post-it")) === false) && (($(e.target).hasClass("header")) === false) && (($(e.target).hasClass("content")) === false) && (($(e.target).hasClass("close")) === false)){
+    if ((($(event.target).hasClass("post-it")) === false) && (($(event.target).hasClass("header")) === false) && (($(event.target).hasClass("content")) === false) && (($(event.target).hasClass("close")) === false)){
 
       id = increment.value
 
+      newPostIt = "<div class='header'><span class='erase'>E</span><span class='close'>X</span></div>"
 
       $("#board").append("<div id='" + id + "' class='post-it'>");
 
       $("#"+id).draggable({scroll: true, scrollSensitivity: 20, scrollSpeed: 20});
 
-      $("#"+id).html("<div class='header'><span class='close'>[X]</span></div>")
+      $("#"+id).html(newPostIt)
 
       $("#"+id).append("<div class='content' contenteditable='true'>")
 
-      changePostItPos(id,e.clientX,e.clientY)
+      changePostItPos(id,event.clientX,event.clientY)
 
       actions = ["clip","blind","bounce","explode","highlight", "shake", "scale", "pulsate", "slide", "size", "fold", "puff"]
 
@@ -66,12 +67,14 @@ function initialize() {
 
       increment.count()
     }
-    else if ($(e.target).hasClass("close") === true) {
-      $(e.target).parent().parent().remove()
+    else if ($(event.target).hasClass("close") === true) {
+      // $(event.target).parent().parent().remove()
+      $(event.target).parents(":eq(1)").fadeOut({duration:1000})
+      $(event.target).parents(":eq(1)").fadeIn({duration:1000})
+      //erase text with fadeOut on click
     }
     else {
-      console.log(e.target)
-      $(e.target).focus()
+      $(event.target).focus()
     }
   })
 
