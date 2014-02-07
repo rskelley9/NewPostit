dc04b58e8885d95a1bfcd506ce59f5cbcf07ba5b
Ryan Kelley <rskelley9@gmail.com>
2014-02-06 20:11:32 -0500

    added individual note font change functionality

--- post-it.js
+++ post-it.js
@@ -7,10 +7,10 @@ var showPostIt = function(id,array) {
   var randomAction = getAction(array)
   $("#"+id).show(randomAction)
   $("#"+id).resizable({
-      animate: true,
-      alsoResize: ".content",
-      aspectRatio: 1 / 1
-    });
+    animate: true,
+    alsoResize: ".content",
+    aspectRatio: 1 / 1
+  });
 }
 
 var movePostIt = function(id,x,y) {
@@ -30,11 +30,11 @@ function initialize() {
 
   $(document).click(function(event) {
     // if there click target isn't a post-it, nor the span.close area then initialize postit
-    if ((($(event.target).hasClass("post-it")) === false) && (($(event.target).hasClass("header")) === false) && (($(event.target).hasClass("close")) === false) && (($(event.target).hasClass("erase")) === false) && ($(event.target).is('#controls') === false) && ($(event.target).hasClass("content") === false) && ($(event.target).is("palette") === false) && ($(event.target).is("#pen-font") === false) && ($(event.target).is("#pen-color") === false)) {
+    if ((($(event.target).hasClass("post-it")) === false) && (($(event.target).hasClass("header")) === false) && (($(event.target).hasClass("note-font")) === false) && (($(event.target).hasClass("close")) === false) && (($(event.target).hasClass("erase")) === false) && ($(event.target).is('#controls') === false) && ($(event.target).hasClass("content") === false) && ($(event.target).is("palette") === false) && ($(event.target).is("#pen-font") === false) && ($(event.target).is("#pen-color") === false)) {
 
       id = count.value
 
-      newPostIt = "<div class='header'><span class='erase'>Erase</span><span class='close'>X</span></div>"
+      newPostIt = "<div class='header'><span class='erase'>Erase</span><span class='note-font'>Font</span><span class='close'>X</span></div>"
 
       $("#board").append("<div id='" + id + "' class='post-it'>");
 
@@ -63,6 +63,31 @@ function initialize() {
     {
       $(event.target).parent().siblings().first().wrapInner( "<div class='fade'></div>" )
       $('.fade').fadeOut({duration:800})
+    }
+    else if ($(event.target).hasClass("note-font") === true)
+    {
+      var noteContent = $(event.target).parent().siblings().first()
+
+      if (noteContent.hasClass('marker'))
+      {
+        noteContent.removeClass('marker').addClass('pen')
+      }
+      else if (noteContent.hasClass('pen'))
+      {
+        noteContent.removeClass('pen').addClass('ink')
+      }
+      else if (noteContent.hasClass('ink'))
+      {
+        noteContent.removeClass('ink').addClass('hand-write')
+      }
+      else if (noteContent.hasClass('hand-write'))
+      {
+        noteContent.removeClass('hand-write')
+      }
+      else
+      {
+        noteContent.addClass('marker')
+      }
     }
     else {
       $(event.target).focus()
