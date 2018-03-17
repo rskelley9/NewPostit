3e056d9875f1ea44ccd5472b3d1ec91fb0a52327
Ryan S Kelley <rskelley9@gmail.com>
2018-03-17 04:06:17 -0400

    line endings

--- controls.js
+++ controls.js
@@ -3,34 +3,33 @@ $(document).ready(function(){
 
     if($('.content').hasClass('marker'))
     {
-    $('.content').removeClass('marker').addClass('pen')
+    $('.content').removeClass('marker').addClass('pen');
     }
     else if ($('.content').hasClass('pen'))
     {
-      $('.content').removeClass('pen').addClass('ink')
+      $('.content').removeClass('pen').addClass('ink');
     }
     else if ($('.content').hasClass('ink'))
     {
-      $('.content').removeClass('ink').addClass('hand-write')
+      $('.content').removeClass('ink').addClass('hand-write');
     }
     else if ($('.content').hasClass('hand-write'))
     {
-      $('.content').removeClass('hand-write')
+      $('.content').removeClass('hand-write');
     }
-    else
-    {
-      $('.content').addClass('marker')
+    else{
+      $('.content').addClass('marker');
     }
   });
 
 var noteColors = ['#f1e09c', '#F1900C', '#39F10C', '#3ce3f5'],
-counter = 0
+counter = 0;
 
 var colors = ['#0a0a0a', '#ef2404', '#2103a9', '#2e9503', '#ff0069', '#4c4c4c', '#0aff3e'],
-counter = 0
+counter = 0;
 
   $('#pen-color').on('click', function(){
         counter = (counter + 1) % colors.length;
-    $('.content').css({"color": colors[counter]})
+    $('.content').css({"color": colors[counter]});
   });
 });
