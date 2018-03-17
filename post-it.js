44764c547589d608761193eee3b69e813ef10268
Ryan S Kelley <rskelley9@gmail.com>
2018-03-17 04:12:35 -0400

    formatting

--- post-it.js
+++ post-it.js
@@ -30,11 +30,22 @@ function initialize() {
 
   $(document).click(function(event) {
     // if there click target isn't a post-it, nor the span.close area then initialize postit
-    if ((($(event.target).hasClass("post-it")) === false) && (($(event.target).hasClass("header")) === false) && (($(event.target).hasClass("note-font")) === false) && (($(event.target).hasClass("close")) === false) && (($(event.target).hasClass("erase")) === false) && ($(event.target).is('#controls') === false) && ($(event.target).hasClass("content") === false) && ($(event.target).is("palette") === false) && ($(event.target).is("#pen-font") === false) && ($(event.target).is("#pen-color") === false)) {
+    if (
+     ($(event.target).hasClass("post-it")) === false) && 
+     (($(event.target).hasClass("header")) === false) && 
+     (($(event.target).hasClass("note-font")) === false) && 
+     (($(event.target).hasClass("close")) === false) && 
+     (($(event.target).hasClass("erase")) === false) && 
+     ($(event.target).is('#controls') === false) && 
+     ($(event.target).hasClass("content") === false) && 
+     ($(event.target).is("palette") === false) && 
+     ($(event.target).is("#pen-font") === false) && 
+     ($(event.target).is("#pen-color") === false
+    ) {
 
       id = count.value;
 
-      newPostIt = "<div class='header'><span class='erase'>Erase</span><span class='note-font'>Font</span><span class='close'>X</span></div>"
+      newPostIt = "<div class='header'><span class='erase'>Erase</span><span class='note-font'>Font</span><span class='close'>X</span></div>";
 
       $("#board").append("<div id='" + id + "' class='post-it'>");
 
@@ -46,46 +57,49 @@ function initialize() {
 
       movePostIt(id,event.clientX,event.clientY);
 
-      effects = ["clip","blind","bounce","explode","highlight", "shake", "scale", "pulsate", "slide", "size", "fold", "puff"];
+      effects = [
+         "clip",
+         "blind",
+         "bounce",
+         "explode",
+         "highlight", 
+         "shake", 
+         "scale", 
+         "pulsate", 
+         "slide", 
+         "size", 
+         "fold", 
+         "puff"
+      ];
 
       showPostIt(id, effects);
 
       count.plusOne();
     }
-    else if ($(event.target).hasClass("close") === true)
-    {
-      $(event.target).parents(":eq(1)").fadeOut({duration:1100})
-      // $(event.target).parents(":eq(1)").fadeIn({duration:1000})
-      //erase text with fadeOut on click
+    else if ($(event.target).hasClass("close") === true) {
+      $(event.target).parents(":eq(1)").fadeOut({duration:1100});
     }
 
-    else if ($(event.target).hasClass("erase") === true)
-    {
+    else if ($(event.target).hasClass("erase") === true) {
       $(event.target).parent().siblings().first().wrapInner( "<div class='fade'></div>" );
       $('.fade').fadeOut({duration:800});
     }
-    else if ($(event.target).hasClass("note-font") === true)
-    {
+    else if ($(event.target).hasClass("note-font") === true) {
       var noteContent = $(event.target).parent().siblings().first()
 
-      if (noteContent.hasClass('marker'))
-      {
+      if (noteContent.hasClass('marker')) {
         noteContent.removeClass('marker').addClass('pen');
       }
-      else if (noteContent.hasClass('pen'))
-      {
+      else if (noteContent.hasClass('pen')) {
         noteContent.removeClass('pen').addClass('ink');
       }
-      else if (noteContent.hasClass('ink'))
-      {
+      else if (noteContent.hasClass('ink')) {
         noteContent.removeClass('ink').addClass('hand-write');
       }
-      else if (noteContent.hasClass('hand-write'))
-      {
+      else if (noteContent.hasClass('hand-write')) {
         noteContent.removeClass('hand-write');
       }
-      else
-      {
+      else {
         noteContent.addClass('marker');
       }
     }
